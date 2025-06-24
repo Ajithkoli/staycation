0a94faba9c5ae51b045d9389a1753ec53b68850f
Ajith koli <ajitkoli302@gmail.com>
2025-06-24 08:20:50 +0530

    created signup page

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
 
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
@@ -58,7 +59,7 @@ app.use(flash());
 //authentications   
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(new LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));//hashing function used by passport -pkbdf
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -68,9 +69,21 @@ app.use((req,res,next)=>{
     res.locals.error=req.flash("error");
     next();
 })
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
 
+// app.get("/demouser",async (req,res)=>{
+//     let fakeUser=new User({
+//         email:"student@gmail.com",
+//         username:"delta-student",
+
+//     });
+
+//     let registeredUser=await User.register(fakeUser,"helloworld");
+//     res.send(registeredUser);
+// })
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 //error handler
---
+++ routes/user.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const router=express.Router();
+const User=require("../models/user.js");
+const wrapAsync = require('../utils/wrapAsync.js');
+
+
+router.get("/signup",(req,res)=>{
+   res.render("./users/signup.ejs");
+}
+)
+
+router.post("/signup",wrapAsync(async (req,res)=>{
+    try{
+            let {username,email,password}=req.body;
+    let newUser=new User({email,username});
+     let registeredUser=await User.register(newUser,password);
+     console.log(registeredUser);
+     req.flash("success","Welcome to wanderlust!");
+     res.redirect("/listings");
+    }
+    catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login",(req,res)=>{
+     res.render("./users/login.ejs");
+})
+
+module.exports=router;
